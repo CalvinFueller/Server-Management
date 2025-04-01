37e2b200130bb074db5fbf97308fb4daebf9ccb2
CalvinFueller <fueller.c@frachtigall.de>
2025-04-01 16:04:56 +0200

    Basic form validations added.

--- BlazorApp1/Models/Server.cs
+++ BlazorApp1/Models/Server.cs
@@ -5,10 +5,12 @@ namespace BlazorApp1.Models
     public class Server
     {
         #region Properties/Fields
+        [Required]
         public int ServerId { get; set; }
         public bool IsOnline { get; set; }
 
         [Required]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters long.")] // Just as a demo to use multiple annotations
         public string? Name { get; set; }
 
         [Required]
